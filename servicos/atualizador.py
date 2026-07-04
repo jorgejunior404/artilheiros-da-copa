b30d01d00ee238a53e4a6282f112f07cfeb7d706
Jorge Junior <jorge13@gmail.com>
2026-07-04 15:03:15 -0300

    feat: adiciona atualizar_tudo() para orquestrar fluxo completo

--- servicos/atualizador.py
+++ servicos/atualizador.py
@@ -1,32 +1,32 @@
 from config import API_KEY, API_BASE_URL
 from servicos.api_client import ApiFootballClient
+from servicos.lista_de_artilheiros import ListaArtilheiros
 from modelos.jogador import Jogador
 
 
 class Atualizador:
-    def __init__(self):
+    def __init__(self, lista_artilheiros: ListaArtilheiros):
         self.client = ApiFootballClient(api_key=API_KEY, base_url=API_BASE_URL)
+        self.lista_artilheiros = lista_artilheiros
 
     def _converter_jogador(self, dado_bruto: dict) -> Jogador:
         info = dado_bruto['player']
         estatisticas = dado_bruto['statistics'][0]
 
-        return Jogador (
+        return Jogador(
             id_api=info['id'],
-            nome= info["name"],
-            selecao= estatisticas["team"]["name"],
-            gols= estatisticas["goals"]["total"]or 0,
-            jogos= estatisticas["games"]["appearences"] or 0
+            nome=info['name'],
+            selecao=estatisticas['team']['name'],
+            gols=estatisticas['goals']['total'] or 0,
+            jogos=estatisticas['games']['appearences'] or 0,
         )
 
-    def atualizar(self) -> list:
+    def atualizar_tudo(self):
+        """Busca os artilheiros na API, atualiza a lista e salva no arquivo."""
         dados_brutos = self.client.buscar_artilheiros(league_id=1, season=2022)
-        top_artilheiros = []
+
         for dado in dados_brutos:
             jogador = self._converter_jogador(dado)
-            top_artilheiros.append(jogador)
+            self.lista_artilheiros.atualizar_jogador(jogador)
 
-        return top_artilheiros
-        
-
-        
+        self.lista_artilheiros.salvar()
